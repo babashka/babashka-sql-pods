05c5d9512713d834c544e2d6c592d4988ed454de
Michiel Borkent <michielborkent@gmail.com>
2020-10-21 16:42:04 +0200

    Bump deps

--- project.clj
+++ project.clj
@@ -9,16 +9,16 @@
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
   :source-paths ["src"]
   :resource-paths ["resources"]
-  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [seancorfield/next.jdbc "1.0.424"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha2"]
+                 [seancorfield/next.jdbc "1.1.610"]
                  [nrepl/bencode "1.1.0"]]
   :profiles {:uberjar {:global-vars {*assert* false}
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
                                   "-Dclojure.spec.skip-macros=true"]
                        :aot :all
                        :main pod.babashka.sql}
-             :feature/postgresql {:dependencies [[org.postgresql/postgresql "42.2.12"]]}
-             :feature/hsqldb {:dependencies [[org.hsqldb/hsqldb "2.4.0"]]}}
+             :feature/postgresql {:dependencies [[org.postgresql/postgresql "42.2.18"]]}
+             :feature/hsqldb {:dependencies [[org.hsqldb/hsqldb "2.5.1"]]}}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
