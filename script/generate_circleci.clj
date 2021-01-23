2df27b7fa8e03b4ebdcd71f5c4366842d7ffb400
Jakub Holy <jakubholy@jakubholy.net>
2021-01-23 13:58:30 +0100

    CI config for oracle (#20)
    * CI config for oracle
    
    * Oracle test: add to workflows
    
    * Update Readme for Oracle
    
    * regenerate CI config

--- script/generate_circleci.clj
+++ script/generate_circleci.clj
@@ -95,13 +95,17 @@ fi" java java java)}}
           :hsqldb-mac  (assoc-in (mac :java 8)
                                  [:environment :POD_DB_TYPE] "hsqldb")
           :postgresql-linux (assoc-in (linux) [:environment :POD_DB_TYPE] "postgresql")
-          :postgresql-mac  (assoc-in (mac) [:environment :POD_DB_TYPE] "postgresql")),
+          :postgresql-mac  (assoc-in (mac) [:environment :POD_DB_TYPE] "postgresql")
+          :oracle-linux (assoc-in (linux) [:environment :POD_DB_TYPE] "oracle")
+          :oracle-mac (assoc-in (mac) [:environment :POD_DB_TYPE] "oracle")),
    :workflows (ordered-map
                :version 2
                :ci {:jobs ["hsqldb-linux"
                            "hsqldb-mac"
                            "postgresql-linux"
-                           "postgresql-mac"]})))
+                           "postgresql-mac"
+                           "oracle-linux"
+                           "oracle-mac"]})))
 
 (require '[clj-yaml.core :as yaml])
 (spit ".circleci/config.yml"
---
+++ test/pod/babashka/oracle_test.clj
@@ -0,0 +1,4 @@
+(ns pod.babashka.oracle-test
+  (:require [clojure.test :refer :all]))
+
+;; Nothing here until we figure out how to get an in-memory / test Oracle DB
