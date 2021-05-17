6fd2ab38ef9c46255a2388970d6bbd1280294483
Michiel Borkent <michielborkent@gmail.com>
2021-05-17 12:25:11 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -9,8 +9,8 @@
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
   :source-paths ["src"]
   :resource-paths ["resources"]
-  :dependencies [[org.clojure/clojure "1.10.2"]
-                 [seancorfield/next.jdbc "1.1.613"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.github.seancorfield/next.jdbc "1.2.659"]
                  [nrepl/bencode "1.1.0"]
                  [com.cognitect/transit-clj "1.0.324"]
                  [cheshire/cheshire "5.10.0"]]
@@ -19,11 +19,11 @@
                                   "-Dclojure.spec.skip-macros=true"]
                        :aot :all
                        :main pod.babashka.sql}
-             :feature/postgresql {:dependencies [[org.postgresql/postgresql "42.2.18"]]}
+             :feature/postgresql {:dependencies [[org.postgresql/postgresql "42.2.20"]]}
              :feature/mssql {:dependencies [[com.microsoft.sqlserver/mssql-jdbc "9.2.0.jre11"]]}
-             :feature/hsqldb {:dependencies [[org.hsqldb/hsqldb "2.5.1"]]}
+             :feature/hsqldb {:dependencies [[org.hsqldb/hsqldb "2.6.0"]]}
              :feature/mysql {:dependencies [[mysql/mysql-connector-java "8.0.25"]]}
-             :feature/oracle {:dependencies [[io.helidon.integrations.db/ojdbc "2.2.0"]]}} ; ojdbc10 + GraalVM config, by Oracle
+             :feature/oracle {:dependencies [[io.helidon.integrations.db/ojdbc "2.3.0"]]}} ; ojdbc10 + GraalVM config, by Oracle
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
