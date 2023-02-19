c30a923593524f44ed9f314cbba8f136dc8e3251
Michiel Borkent <michielborkent@gmail.com>
2023-02-19 16:45:32 +0100

    this should work?

--- script/generate_circleci.clj
+++ script/generate_circleci.clj
@@ -103,8 +103,8 @@ fi" java java java)}}
                               :command "bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install) --dir $(pwd)"}}
                        {:run {:name "Fix ssl libs for tests"
                               :command "
-sudo ln -s /usr/lib/libssl.dylib /usr/local/opt/openssl/lib/libssl.1.0.0.dylib\n
-sudo ln -s /usr/lib/libcrypto.dylib /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib\n
+sudo ln -s /usr/local/opt/openssl@3/lib/libssl.1.0.0.dylib /usr/local/opt/openssl/lib/libssl.1.0.0.dylib\n
+sudo ln -s /usr/local/opt/openssl@3/lib/libcrypto.1.0.0.dylib /usr/local/opt/openssl/lib/libssl.1.0.0.dylib\n
 "}}
                        {:run {:name "Build binary",
                               :command "./bb script/compile.clj",
